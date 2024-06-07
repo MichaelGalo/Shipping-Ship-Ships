ec7150d20bb33f1a552de4b0e68b779050f4cbc1
Michael Galo <michael@michaelthinks.co>
2024-06-07 11:40:33 -0500

    haulers portion created

---
+++ scripts/cargo.js
@@ -0,0 +1,15 @@
+import { getHaulers } from "./database.js";
+
+export const HaulerList = () => {
+  const haulers = getHaulers();
+
+  let haulerHTML = "<ul>";
+
+  for (const hauler of haulers) {
+    haulerHTML += `<li>${hauler.name}</li>`;
+  }
+
+  haulerHTML += "</ul>";
+
+  return haulerHTML;
+};
--- scripts/database.js
+++ scripts/database.js
@@ -5,8 +5,44 @@ export const database = {
     { id: 3, location: "Rotterdam, The Netherlands", volume: "14.35" },
     { id: 4, location: "Antwerp, Belgium", volume: "12.04" },
   ],
+  haulers: [
+    {
+      id: 1,
+      name: "The Knotty Buoy",
+      transportShip: transportShip.id,
+      weight: 10000,
+    },
+    {
+      id: 2,
+      name: "The Ship Faced",
+      transportShip: transportShip.id,
+      weight: 50000,
+    },
+    {
+      id: 3,
+      name: "The Berth Control",
+      transportShip: transportShip.id,
+      weight: 80000,
+    },
+    {
+      id: 4,
+      name: "The Haulin' Oats",
+      transportShip: transportShip.id,
+      weight: 150000,
+    },
+    {
+      id: 5,
+      name: "The Reefer Madness",
+      transportShip: transportShip.id,
+      weight: 300000,
+    },
+  ],
 };
 
 export const getDocks = () => {
   return database.docks.map((dock) => ({ ...dock }));
+};
+
+export const getHaulers = () => {
+  return database.haulers.map((hauler) => ({ ...hauler }));
 };
