9e766df5196d5ac6751a7af82aa60e97c129af16
Michael Galo <michael@michaelthinks.co>
2024-06-07 14:44:41 -0500

    added docks event

--- scripts/docks.js
+++ scripts/docks.js
@@ -1,4 +1,24 @@
-import { getDocks } from "./database.js";
+import { getDocks, getTransports } from "./database.js";
+
+const Transports = getTransports();
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  let currentlyUnloading = "";
+
+  if (itemClicked.dataset.type === "dock") {
+    for (const transport of Transports) {
+      if (parseInt(itemClicked.dataset.id) === transport.targetDock) {
+        currentlyUnloading = transport.name;
+      }
+    }
+
+    window.alert(
+      `The ${itemClicked.dataset.location} dock is currently unloading ${currentlyUnloading}`
+    );
+  }
+});
 
 export const DockList = () => {
   const docks = getDocks();
@@ -6,7 +26,7 @@ export const DockList = () => {
   let docksHTML = "<ul>";
 
   for (const dock of docks) {
-    docksHTML += `<li>${dock.location}</li>`;
+    docksHTML += `<li data-type="dock" data-id=${dock.id} data-location=${dock.location}>${dock.location}</li>`;
   }
 
   docksHTML += "</ul>";
